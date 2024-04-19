aa0129d7e97cccf403ca22d6475c96dde1fb73b7
AliBaBas02 <kostasstilianou@gmail.com>
2024-04-19 17:01:00 +0300

    Revert "Updated Card.vue, Vault.vue, tailwind.config.js"
    This reverts commit b514da6b0cc47583cd4a50937c9dfad650b73c3a.
    
    dsadsa

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,6 @@ module.exports = {content: [
       },
       transitionDuration: {
         '5000': '5000ms',
-      },
-      colors: {
-        eggplant: {
-          500: "#424458",
-          600: "#373949"
-        }
       }
     },
   },
